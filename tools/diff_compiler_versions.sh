5e80ab046be00935a23b05e98e9d03627a9ab79b
Simon Spies <sspies@janestreet.com>
2025-08-13 15:55:42 +0100

    Configure flag for emitting assembly on compiler (#4453)

--- tools/diff_compiler_versions.sh
+++ tools/diff_compiler_versions.sh
@@ -87,16 +87,14 @@ git archive --format=tar --prefix=revision/ $REVISION | (cd $BUILDDIR && tar xf 
 
 # we first build the bootcompiler of the new compiler
 cd $REVISION_DIR
-sed -i.bak "s/echo '(:standard \$(if \$(filter true,\$(FUNCTION_SECTIONS)),-function-sections,))' > ocamlopt_flags.sexp/echo '(:standard -S \$(if \$(filter true,\$(FUNCTION_SECTIONS)),-function-sections,))' > ocamlopt_flags.sexp/g" Makefile.common-ox && rm Makefile.common-ox.bak
 $AUTOCONF
-./configure --enable-ocamltest --enable-warn-error --prefix="$INSTALL_PATH"
+./configure --enable-ocamltest --enable-warn-error --enable-keep-compiler-asm --prefix="$INSTALL_PATH"
 make boot-compiler
 
 # we turn to the base line compiler and build the normal version
 cd $BASE_ORIGINAL_DIR
-sed -i.bak "s/echo '(:standard \$(if \$(filter true,\$(FUNCTION_SECTIONS)),-function-sections,))' > ocamlopt_flags.sexp/echo '(:standard -S \$(if \$(filter true,\$(FUNCTION_SECTIONS)),-function-sections,))' > ocamlopt_flags.sexp/g" Makefile.common-ox && rm Makefile.common-ox.bak
 $AUTOCONF
-./configure --enable-ocamltest --enable-warn-error --prefix="$INSTALL_PATH"
+./configure --enable-ocamltest --enable-warn-error --enable-keep-compiler-asm --prefix="$INSTALL_PATH"
 make _install
 cp -R -f _install/. "$TARGETDIR/base-compiler-original/_install/"
 cp -R -f _build/. "$TARGETDIR/base-compiler-original/_build/"
@@ -104,9 +102,8 @@ cp -R -f _build/. "$TARGETDIR/base-compiler-original/_build/"
 
 # we build a version with the new compiler
 cd $BASE_REVISION_DIR
-sed -i.bak "s/echo '(:standard \$(if \$(filter true,\$(FUNCTION_SECTIONS)),-function-sections,))' > ocamlopt_flags.sexp/echo '(:standard -S \$(if \$(filter true,\$(FUNCTION_SECTIONS)),-function-sections,))' > ocamlopt_flags.sexp/g" Makefile.common-ox && rm Makefile.common-ox.bak
 $AUTOCONF
-./configure --enable-ocamltest --enable-warn-error --prefix="$INSTALL_PATH"
+./configure --enable-ocamltest --enable-warn-error --enable-keep-compiler-asm --prefix="$INSTALL_PATH"
 make boot-compiler
 # hack: we copy over the boot compiler from the revision compiler
 cp -L -R -f "$REVISION_DIR/_build/_bootinstall/bin/ocamlopt.opt" _build/_bootinstall/bin/
