3510f462cab64c8d6f433ead3a592bd4efd2fdf4
usuyus <yusufonurusumez@gmail.com>
2025-08-13 17:35:40 +0100

    update tailcall tests

--- oxcaml/tests/backend/llvmize/tailcall.ml
+++ oxcaml/tests/backend/llvmize/tailcall.ml
@@ -18,12 +18,12 @@ and collatz_even n cnt =
   then collatz_even (n / 2) (cnt + 1)
   else collatz_odd n cnt
 
-(* CR yusumez: This test is broken since LLVM doesn't like a mismatch in
-   parameter count. I suppose we can check whether its calling convention is
-   ocamlcc and skip the check if so. *)
-(* let tail_call_outside n x = Tailcall2.tail_call_me (n + 1) (2 * n) x *)
+(* This makes sure that caml_apply is treated correctly. LLVM complains when
+   parameter counts mismatch for tail calls, but it should be fine in our case.
+   We skip that check in LLVM if the calling conventions are OCaml's. *)
+let tail_call_outside n x = Tailcall2.tail_call_me (n + 1) (2 * n) x
 
 let () =
   Format.printf "fib_general: %d\n" (fib_general ~a0:3 ~a1:7 5);
-  Format.printf "collatz_odd: %d\n" (collatz_odd 27 0)
-(* tail_call_outside 37 41.3 *)
+  Format.printf "collatz_odd: %d\n" (collatz_odd 27 0);
+  tail_call_outside 37 41.3
