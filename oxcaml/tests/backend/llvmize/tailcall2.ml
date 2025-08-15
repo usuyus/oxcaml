28dfa6a21b27f03dc7a115892cfea2b862a0e82f
usuyus <yusufonurusumez@gmail.com>
2025-08-15 12:04:03 +0100

    tailcall prologue test

--- oxcaml/tests/backend/llvmize/tailcall2.ml
+++ oxcaml/tests/backend/llvmize/tailcall2.ml
@@ -1,1 +1,4 @@
-let tail_call_me n m x = Format.printf "tail_call_me: %d %d %f\n" n m x
+let[@local never] [@inline never] tail_call_me n m x =
+  Format.printf "tail_call_me: %d %d %f\n" n m x
+
+let[@local never] [@inline never] do_nothing () = ()
